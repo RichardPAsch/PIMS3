8d00e5e4ae24b2c96172316ee78d0dd82fa26e35
Richard P. Asch <rpasch@rpclassics.net>
2018-10-19 11:01:38 -0700

    Testing.

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace PIMS3
 {
     public class Startup
