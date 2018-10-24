90cb84b23f00379542b1ff48e21ce100d9011bd1
Richard P. Asch <rpasch@rpclassics.net>
2018-10-24 11:39:01 -0700

    Initial creation

---
+++ ClientApp/src/app/data-import/data-import.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+  selector: 'data-import',
+  templateUrl: './data-import.component.html'
+})
+
+export class DataImportComponent {
+
+}
