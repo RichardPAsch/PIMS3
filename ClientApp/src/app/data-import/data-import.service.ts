e4c7e85431a6cafede8756dbaa52086d1d7db971
Richard P. Asch <rpasch@rpclassics.net>
2018-10-31 17:29:23 -0700

    postImportFileData() - WIP

--- ClientApp/src/app/data-import/data-import.service.ts
+++ ClientApp/src/app/data-import/data-import.service.ts
@@ -1,17 +1,41 @@
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DataImportVm } from './data-importVm';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+
 
 @Injectable()
 
 export class DataImportService {
+    private _http: HttpClient;
+    private url: string;
 
     constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-
+        this._http = http;
+        this.url = baseUrl;
     }
 
+    /* All HttpClient methods return an RxJS Observable of something.
+       Also see: https://angular.io/tutorial/toh-pt6
+    */
 
-    postRevenueData() {
+    postImportFileData(importFileToProcess: DataImportVm): Observable<DataImportVm> {
+        //Send to backend for processing here: WIP -> ImportFileController.cs / ImportFileControllerReference.txt
 
+        // Also see: https://stackblitz.com/angular/nqyqljajkrp?file=src%2Fapp%2Fhero.service.ts
+        return this._http.post<DataImportVm>(this.url + 'api/Income/GetRevenueSummary', DataImportVm, httpOptions)
+            .pipe(); // WIP TODO
+
+        //return http.post<DataImportVm>(baseUrl + 'api/Income/GetRevenueSummary', DataImportVm, httpOptions)
+        //           .pipe(tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+        //    catchError(this.handleError<Hero>('addHero'))
+        //);
+ 
 
     }
 
