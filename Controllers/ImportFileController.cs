8895e5a2be390357bfa1ab6e116b8bd408b2392e
Richard P. Asch <rpasch@rpclassics.net>
2018-11-03 14:42:39 -0700

    WIP.  - ProcessImportFile().

--- Controllers/ImportFileController.cs
+++ Controllers/ImportFileController.cs
@@ -20,9 +20,61 @@ namespace PIMS3.Controllers
     [ApiController]
     public class ImportFileController : ControllerBase
     {
+        /*
+        public async Task<IHttpActionResult> ProcessImportFile([FromBody] ImportFileVm importFile)
+        {
+            string dataPersistenceResults;
+            var importFileUrl = importFile.ImportFilePath;
+            var requestUri = Request.RequestUri.AbsoluteUri;
+
+            _serverBaseUri = Utilities.GetWebServerBaseUri(requestUri);
+
+            // Verify investor login via email addr.
+            _currentInvestor = _identityService.CurrentUser;
+            if (_currentInvestor == null)
+            {
+                //return BadRequest("Import aborted; Investor login required."); 
+                // un-comment during Fiddler testing
+                // TODO: in Production, exit if not logged in.
+                _currentInvestor = "rpasch@rpclassics.net";
+            }
+
+            if (importFile.IsRevenueData)
+            {
+                var assetCtrl = new AssetController(_repositoryAsset, _identityService, _repositoryInvestor);
+                var investorId = Utilities.GetInvestorId(_repositoryInvestor, _currentInvestor);
+                _existingInvestorAssets = await assetCtrl.GetByInvestorAllAssets(investorId) as OkNegotiatedContentResult<List<AssetIncomeVm>>;
+                var portfolioRevenueToBeInserted = ParseRevenueSpreadsheet(importFileUrl);
+                if (portfolioRevenueToBeInserted == null)
+                    return BadRequest("Invalid XLS data submitted.");
+
+                var revenueToBeInserted = portfolioRevenueToBeInserted as Income[] ?? portfolioRevenueToBeInserted.ToArray();
+                if (!revenueToBeInserted.Any())
+                    return BadRequest("No income data saved; please check ticker symbol(s), amount(s), and/or account(s) for validity.");
+
+                dataPersistenceResults = PersistIncomeData(revenueToBeInserted);
+            }
+            else
+            {
+                var portfolioListing = ParsePortfolioSpreadsheet(importFileUrl);
+                if (portfolioListing == null)
+                    return BadRequest("Error processing Position(s) in one or more accounts.");
+
+                var portfolioAssetsToBeInserted = portfolioListing as AssetCreationVm[] ?? portfolioListing.ToArray();
+                if (!portfolioAssetsToBeInserted.Any())
+                    return BadRequest("Invalid XLS data, duplicate position-account ?");
+
+                dataPersistenceResults = PersistPortfolioData(portfolioAssetsToBeInserted);
+            }
+
+            var responseVm = new HttpResponseVm { ResponseMsg = dataPersistenceResults };
+            return Ok(responseVm);
+        }
+
+        */
     }
 
-
+    
 
 
 }
