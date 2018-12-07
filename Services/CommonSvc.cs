4f1f83c9ab187f575f0943b3b839aa28838b5f55
Richard P. Asch <rpasch@rpclassics.net>
2018-12-06 16:34:11 -0800

    Added GetInvestorIdFromInvesto() in lieu of repository use.

--- Services/CommonSvc.cs
+++ Services/CommonSvc.cs
@@ -1,25 +1,28 @@
 ﻿using PIMS3.Data;
 using PIMS3.Data.Repositories;
 using System;
+using System.Linq;
+
 
 namespace PIMS3.Services
 {
     public class CommonSvc : ICommonSvc
     {
         private readonly PIMS3Context _ctx;
-        //private readonly InvestorRepository _repo;
 
-        public CommonSvc(PIMS3Context ctx) //, InvestorRepository repo)
+        public CommonSvc(PIMS3Context ctx) 
         {
             _ctx = ctx;
-           // _repo = repo;
         }
 
 
-        public string GetInvestorIdFromInvestor(string currentInvestor)
+        public string GetInvestorIdFromInvestor(string investorEMail)
         {
-            //return _repo.RetreiveIdByName(currentInvestor);
-            return "";
+            // investorEMail aka login name.
+            return _ctx.Investor
+                       .Where(investor => investor.EMailAddr.Trim() == investorEMail.Trim())
+                       .FirstOrDefault().InvestorId
+                       .ToString();
         }
 
         // From: C:\Development\PIMS_References\PIMS.WebApi\Utilities.cs
