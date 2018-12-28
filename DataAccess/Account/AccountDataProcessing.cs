2ab1bd54cd5259c6122cdddff15a2c9ccd13825f
Richard P. Asch <rpasch@rpclassics.net>
2018-12-28 13:01:16 -0800

    Initial creation.

---
+++ DataAccess/Account/AccountDataProcessing.cs
@@ -0,0 +1,29 @@
+﻿using PIMS3.Data;
+using System.Linq;
+
+namespace PIMS3.DataAccess.Account
+{
+    public class AccountDataProcessing
+    {
+        private PIMS3Context _ctx;
+
+        public AccountDataProcessing(PIMS3Context ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IQueryable<string> GetAccountTypeId(string acctDesc)
+        {
+            return _ctx.AccountType.Where(a => a.AccountTypeDesc.Trim() == acctDesc.Trim())
+                                   .Select(a => a.AccountTypeId)
+                                   .AsQueryable();
+
+        }
+
+
+        
+
+
+
+    }
+}
