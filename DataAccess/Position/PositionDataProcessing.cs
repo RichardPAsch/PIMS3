9d375a95d3ea4e898cc7731b84f29d59f3aebdd8
Richard P. Asch <rpasch@rpclassics.net>
2018-12-28 12:57:19 -0800

    Initial creation.

---
+++ DataAccess/Position/PositionDataProcessing.cs
@@ -0,0 +1,32 @@
+﻿using PIMS3.Data;
+using System.Linq;
+
+namespace PIMS3.DataAccess.Position
+{
+    public class PositionDataProcessing
+    {
+        private PIMS3Context _ctx;
+
+        public PositionDataProcessing(PIMS3Context ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+
+        public bool GetPositionByTickerAndAccount(string tickerSymbol, string assetAccount)
+        {
+            // Temporary until security implemented.
+            var currentInvestorId = "CF256A53-6DCD-431D-BC0B-A810010F5B88"; 
+
+            var positionInfo = _ctx.Position
+                                   .Where(p => p.PositionAsset.InvestorId == currentInvestorId &&
+                                               p.PositionAsset.Profile.TickerSymbol == tickerSymbol &&
+                                               p.AccountType.AccountTypeDesc.ToUpper() == assetAccount.ToUpper())
+                                   .Select(p => p.PositionAsset)
+                                   .AsQueryable();
+
+            return positionInfo.Any();
+        }
+    }
+}
