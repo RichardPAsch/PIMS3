ad4abce732b3f6e6b359a153745e9b32dc5185b3
Richard P. Asch <rpasch@rpclassics.net>
2019-02-11 10:17:13 -0800

    Initial creation.

---
+++ ClientApp/src/app/income-projections/profile.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Model class for 1:1 mapping in grid.
+var Profile = /** @class */ (function () {
+    function Profile() {
+    }
+    return Profile;
+}());
+exports.Profile = Profile;
+//# sourceMappingURL=profile.js.map
---
+++ ClientApp/src/app/income-projections/profile.ts
@@ -0,0 +1,12 @@
+
+// Model class for 1:1 mapping in grid.
+export class Profile {
+
+    ticker: string;
+    capital: number;
+    unitPrice: number;
+    dividendRate: number;
+    dividendYield: number;
+    projectedMonthlyIncome: number;
+
+}
