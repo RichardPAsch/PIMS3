a288418b0621f8032638fbb8092f2c2a2f944159
Richard P. Asch <rpasch@rpclassics.net>
2019-02-11 10:07:40 -0800

    Clean up.

--- ClientApp/src/app/message.service.ts
+++ ClientApp/src/app/message.service.ts
@@ -3,14 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class MessageService {
-  messages: string[] = [];
 
-  add(message: string) {
-    this.messages.push(message);
-  }
+      messages: string[] = [];
 
-  clear() {
-    this.messages = [];
-    }
+
+      add(message: string) {
+        this.messages.push(message);
+      }
+
+
+      clear() {
+        this.messages = [];
+      }
 
 }
