60d7d27ded6ad1697def4d13d939523f67581322
Richard P. Asch <rpasch@rpclassics.net>
2019-02-13 14:49:09 -0800

    Updated getProjections() to handle manual overrides in calculating income projections.

--- ClientApp/src/app/income-projections/income-projections.component.ts
+++ ClientApp/src/app/income-projections/income-projections.component.ts
@@ -49,35 +49,52 @@ export class IncomeProjectionsComponent implements OnInit {
         let profiles = new Array<Profile>();
         var selectedNodes = this.agGrid.api.getSelectedNodes();
         var selectedData = selectedNodes.map(node => node.data);
+                
 
         for (let gridRow = 0; gridRow < selectedData.length; gridRow++)
         {
             if (selectedData[gridRow].ticker == "" || selectedData[gridRow].capital == 0) {
                 alert("Error processing projection(s): \nmissing 'ticker' and/or 'capital' entry(ies).");
                 return;
-            }
-
-            this.profileSvc.getProfileData(selectedData[gridRow].ticker)
-                .retry(2)     // Retrying request in case of transient errors, e.g, slow network, no internet access.
-                .subscribe(responseProfile => {
-                                    profiles.push(this.initializeGridModel(responseProfile, selectedData[gridRow].capital));
-                                    this.agGrid.api.setRowData(profiles);
-                },
-                (apiErr: HttpErrorResponse) => {
-                    if (apiErr.error instanceof Error) {
-                        // Client-side or network error encountered.
-                        alert("Error processing projection(s): \network or application error. Please try later.");
-                    }
-                    else {
-                        //API returns unsuccessful response status codes, e.g., 404, 500 etc.
-                        let truncatedMsgLength = apiErr.error.errorMsg.indexOf(":") - 7;
-                        alert("Error processing projection(s): due to : \n" + apiErr.error.errorMsg.substring(0, truncatedMsgLength)
-                                                                            + "."
-                                                                            + "\nCheck ticker validity.");
-                    }
+            } else {
+                // Check for manual entries, bypassing web-based fetched profile data for calculations, e.g., due 
+                // to no, or incomplete data available via web service.
+                if (selectedData[gridRow].unitPrice > 0 && selectedData[gridRow].dividendRate > 0) {
+                    // Calculate projection based on submitted entries.
+                    let manualProfile: { [key: string]: any } = {
+                        tickerSymbol: selectedData[gridRow].ticker,
+                        unitPrice: selectedData[gridRow].unitPrice,
+                        dividendRate: selectedData[gridRow].dividendRate,
+                        dividendYield: selectedData[gridRow].dividendYield > 0 ? selectedData[gridRow].dividendYield : 0
+                    };
+                    profiles.push(this.initializeGridModel(manualProfile, selectedData[gridRow].capital));
+                    // Last row ?
+                    if (gridRow == selectedData.length - 1 )
+                        this.agGrid.api.setRowData(profiles);
+                } else {
+                    this.profileSvc.getProfileData(selectedData[gridRow].ticker)
+                        .retry(2)     // Retrying request in case of transient errors, e.g, slow network, no internet access.
+                        .subscribe(responseProfile => {
+                            profiles.push(this.initializeGridModel(responseProfile, selectedData[gridRow].capital));
+                            this.agGrid.api.setRowData(profiles);
+                        },
+                            (apiErr: HttpErrorResponse) => {
+                                if (apiErr.error instanceof Error) {
+                                    // Client-side or network error encountered.
+                                    alert("Error processing projection(s): \network or application error. Please try later.");
+                                }
+                                else {
+                                    //API returns unsuccessful response status codes, e.g., 404, 500 etc.
+                                    let truncatedMsgLength = apiErr.error.errorMsg.indexOf(":") - 7;
+                                    alert("Error processing projection(s): due to : \n" + apiErr.error.errorMsg.substring(0, truncatedMsgLength)
+                                        + "."
+                                        + "\nCheck ticker validity.");
+                                }
+                            }
+                        ); // end subscribe
                 }
-            ); // end subscribe
-        }
+            }
+        } // end for
     }
 
 
@@ -97,6 +114,7 @@ export class IncomeProjectionsComponent implements OnInit {
         // Calculating projected income (bus. logic) done here, as projectedIncome is not a 'Profile.cs' attribute.
         let profileRecord = new Profile();
         profileRecord.ticker = recvdProfile.tickerSymbol;
+        profileRecord.ticker = profileRecord.ticker.toUpperCase();
         profileRecord.capital = capitalToInvest;
         profileRecord.unitPrice = recvdProfile.unitPrice;
         profileRecord.dividendRate = recvdProfile.dividendRate;
