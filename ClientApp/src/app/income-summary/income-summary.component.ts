33b346efaf17c4c44b25f02324517304e34c0ffe
Richard P. Asch <rpasch@rpclassics.net>
2019-02-15 16:07:48 -0800

    Updated processIncomeSummary(), for error handling & calculations. teted Ok.

--- ClientApp/src/app/income-summary/income-summary.component.ts
+++ ClientApp/src/app/income-summary/income-summary.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { CurrencyPipe } from '@angular/common';
+import 'rxjs/add/operator/retry'; 
 
 @Component({
   selector: 'income-summary-data',
@@ -8,22 +9,55 @@ import { CurrencyPipe } from '@angular/common';
 })
 
 export class IncomeSummaryComponent {
-  private currentDate: Date = new Date();
-  currentYear: number = this.currentDate.getFullYear();
-  public ytdIncomeSummary: IncomeSummary[];
+    private currentDate: Date = new Date();
+    private currentYear: number = this.currentDate.getFullYear();
+    public currentYearHeading: number = this.currentYear;
+    public ytdIncomeSummary: IncomeSummary[];
+    private apiUrl: string;
+    private httpCilentReference: HttpClient;
+    public incomeSummaryTotal: any;
 
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<IncomeSummary[]>(baseUrl + 'api/Income/GetRevenueSummary')
-      .subscribe(result => { this.ytdIncomeSummary = result; },
-        error => console.error("IncomeSummaryComponent ctor error: ", error));
-  }
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string)
+    {
+        this.apiUrl = baseUrl + 'api/Income/' + '0';
+        this.httpCilentReference = http;
+        this.processIncomeSummary();
+    }
+
+    onYearSelected(yearsBackDated: number) {
+        this.currentYearHeading = this.currentYear - yearsBackDated;
+        this.apiUrl = this.apiUrl.substr(0, this.apiUrl.lastIndexOf("/") + 1) + yearsBackDated.toString();
+        this.processIncomeSummary();
+    }
+
+    processIncomeSummary() {
+
+        this.httpCilentReference.get<any[]>(this.apiUrl)
+            .retry(2)
+            .subscribe(result =>
+            {
+                this.ytdIncomeSummary = result;
+                let runningTotal: any = 0.0;
+                let ctr: any = 0;
+
+                for (ctr in result) {
+                    runningTotal += parseFloat(result[ctr].amountRecvd);
+                }
+                this.incomeSummaryTotal = runningTotal;
+            },
+            (error: HttpErrorResponse) => alert("Error retreiving income data for year: \n"
+                                                    + this.currentYearHeading
+                                                    + "\ndue to " + error.message)
+            );
+    }
+
 }
 
 
 interface IncomeSummary {
   MonthRecvd: number;
-  AmountRecvd: number | CurrencyPipe;
+  AmountRecvd: number;
   YtdAverage: number | CurrencyPipe;
   Rolling3MonthAverage: number | CurrencyPipe; 
 }
