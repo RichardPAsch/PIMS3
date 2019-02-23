9cbf48a4346b748da70374f7b3b8ad9db65c8c1d
Richard P. Asch <rpasch@rpclassics.net>
2019-02-22 17:28:17 -0800

    Added UpdatePositionPymtDueFlags() to handle income schedule.

--- DataAccess/Position/PositionDataProcessing.cs
+++ DataAccess/Position/PositionDataProcessing.cs
@@ -1,5 +1,9 @@
 ﻿using PIMS3.Data;
 using System.Linq;
+using PIMS3.ViewModels;
+using System.Collections.Generic;
+using System;
+
 
 namespace PIMS3.DataAccess.Position
 {
@@ -11,8 +15,7 @@ namespace PIMS3.DataAccess.Position
         {
             _ctx = ctx;
         }
-
-
+        
 
         public IQueryable<Data.Entities.Asset> GetPositionAssetByTickerAndAccount(string tickerSymbol, string assetAccount)
         {
@@ -29,13 +32,62 @@ namespace PIMS3.DataAccess.Position
         }
 
 
-        public IQueryable<Data.Entities.Position> GetPositionsByInvestorId(string investorId)
+        public bool UpdatePositionPymtDueFlags(string[] positionIds)
         {
-            return _ctx.Position.Where(p => p.PositionAsset.InvestorId == investorId && 
-                                            p.Status == "A" &&
-                                            (p.PymtDue == null || p.PymtDue == false))
-                                .AsQueryable();
+            var updatesAreOk = false;
+            var positionsToUpdate = new List<Data.Entities.Position>();
+            var positionsUpdated = 0;
 
+            // Get matching Positions.
+            foreach (var id in positionIds)
+            {
+                positionsToUpdate.Add(_ctx.Position.Where(p => p.PositionId == id).FirstOrDefault());
+            }
+
+            // Update Positions.
+            if(positionsToUpdate.Count() == positionIds.Length)
+            {
+                foreach(var position in positionsToUpdate)
+                {
+                    position.PymtDue = false;
+                    position.LastUpdate = DateTime.Now;
+                }
+
+                _ctx.UpdateRange(positionsToUpdate);
+                positionsUpdated = _ctx.SaveChanges();
+
+                if(positionsUpdated == positionsToUpdate.Count())
+                    updatesAreOk = true;
+            }
+
+            return updatesAreOk;
+        }
+
+
+        public IQueryable<IncomeReceivablesVm> GetPositionsForIncomeReceivables(string investorId)
+        {
+            var positions = _ctx.Position.Where(p => p.PositionAsset.InvestorId == investorId
+                                                 &&  p.Status == "A" 
+                                                 && (p.PymtDue == null || p.PymtDue == true))
+                                         .AsQueryable();
+
+            var assets = _ctx.Asset.Select(a => a);
+
+            var dataJoin = positions.Join(assets, p => p.AssetId, a => a.AssetId, (positionData, assetData) => new IncomeReceivablesVm
+            {
+                PositionId = positionData.PositionId,
+                TickerSymbol = assetData.Profile.TickerSymbol,
+                AccountTypeDesc = positionData.AccountType.AccountTypeDesc,
+                DividendPayDay = assetData.Profile.DividendPayDay,
+                DividendFreq = assetData.Profile.DividendFreq,
+                DividendMonths = assetData.Profile.DividendMonths
+            });
+
+            return dataJoin.Where(data => data.DividendFreq == "A" || data.DividendFreq == "S" ||
+                                          data.DividendFreq == "Q" || data.DividendFreq == "M")
+                           .OrderBy(data => data.DividendFreq)
+                           .ThenBy(data => data.TickerSymbol)
+                           .AsQueryable();
         }
     }
 }
