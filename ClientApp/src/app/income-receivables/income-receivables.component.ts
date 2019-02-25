accd0a93852c5faf58bb574ac925b5220550e26a
Richard P. Asch <rpasch@rpclassics.net>
2019-02-25 10:59:33 -0800

    Updated processPositionUpdates() for screen refreshes, post-processing. Changed alert message.

--- ClientApp/src/app/income-receivables/income-receivables.component.ts
+++ ClientApp/src/app/income-receivables/income-receivables.component.ts
@@ -91,8 +91,11 @@ export class IncomeReceivablesComponent implements OnInit {
         this.receivablesSvc.UpdateIncomeReceivables(selectedPositionData)
             .retry(2)
             .subscribe(updateResponse => {
-                if (updateResponse)
-                    alert("Update(s) successfull for :" + selectedPositionData.length + " Position(s).");
+                if (updateResponse) {
+                    alert("Successfully marked : " + selectedPositionData.length + " Position(s) as paid.");
+                    // Refresh.
+                    this.getReceivables();
+                }
                 else
                     alert("Error marking Position(s) as payment received, check Position data.");
             },
