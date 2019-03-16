18def2ddf062df8174b4c487523eff6ac47901db
Richard P. Asch <rpasch@rpclassics.net>
2019-03-15 17:44:22 -0700

    Initial creation.

---
+++ ClientApp/src/app/profile/profile.ts
@@ -0,0 +1,12 @@
+export class Profile {
+    tickerSymbol: string;
+    tickerDesc: string;
+    divRate: number;
+    divYield: number;
+    divFreq: string;
+    PE_ratio: number;
+    EPS: number;
+    unitPrice: number;
+    divPayMonths: string;
+
+}
