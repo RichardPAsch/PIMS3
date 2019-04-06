f3465b60261254ba5b86c97093185fad7a25e61a
Richard P. Asch <rpasch@rpclassics.net>
2019-04-06 15:08:51 -0700

    Updated getProfileData() & getProfileDividendInfo(). Added getProfileDataViaDb().

--- ClientApp/src/app/shared/profile.service.ts
+++ ClientApp/src/app/shared/profile.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Profile } from '../income-projections/profile';
+import { ProjectionProfile } from '../income-projections/projection-profile';
+import { Profile } from '../profile/profile';
 
 
 let httpHeaders = new HttpHeaders()
@@ -19,17 +20,31 @@ export class ProfileService {
     constructor(private http: HttpClient) {
     }
 
-    getProfileData(ticker: string): Observable<Profile> {
+    // TODO: change ProjectionProfile -> Profile !!
+    getProfileData(ticker: string): Observable<ProjectionProfile> {
 
         let webApiUri = baseUrl + "/api/Profile/" + ticker;
 
         // Returns Profile as an Observable, to be subscribed to.
-        return this.http.get<Profile>(webApiUri);
+        return this.http.get<ProjectionProfile>(webApiUri);
+    }
+
+    getProfileDataViaDb(ticker: string): Observable<boolean> {
+
+        let webApiUri = baseUrl + "/api/Profile/" + ticker + "/" + true;
+
+        return this.http.get<any>(webApiUri);
     }
 
     getProfileDividendInfo(searchTicker: string): Observable<any> {
 
         let webApiUri = baseUrl + "/api/DivInfo/" + searchTicker;
-        return this.http.get<Profile>(webApiUri);
+        return this.http.get<ProjectionProfile>(webApiUri);
+    }
+
+    updateProfile(partialProfileUpdate: Profile): Observable<boolean> {
+
+        let webApiUri = baseUrl + "/api/Profile";
+        return this.http.put<boolean>(webApiUri, partialProfileUpdate);
     }
 }
