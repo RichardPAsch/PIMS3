c5796fbaa53637ec35d9ec181627c3a91c2e3863
Richard P. Asch <rpasch@rpclassics.net>
2019-04-06 14:58:08 -0700

    Renamed from 'Profile' to avoid ambiguity.

---
+++ ClientApp/src/app/income-projections/projection-profile.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Model class for 1:1 mapping in grid.
+var ProjectionProfile = /** @class */ (function () {
+    function ProjectionProfile() {
+    }
+    return ProjectionProfile;
+}());
+exports.ProjectionProfile = ProjectionProfile;
+//# sourceMappingURL=profile.js.map
---
+++ ClientApp/src/app/income-projections/projection-profile.ts
@@ -0,0 +1,14 @@
+
+// Model class for 1:1 mapping in grid.
+export class ProjectionProfile {
+
+    ticker: string;
+    capital: number;
+    unitPrice: number;
+    dividendRate: number;
+    dividendYield: number;
+    projectedMonthlyIncome: number;
+    dividendFreq: string;
+    dividendPayMonths: string;
+
+}
