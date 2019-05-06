75a85e1e06df4ad30bf04700ca281b351d8c7a42
Richard P. Asch <rpasch@rpclassics.net>
2019-05-06 15:15:34 -0700

    Major revisions to accommodate use of  agGrid as replacement to prior <table> setup.

--- ClientApp/src/app/income-summary/income-summary.component.ts
+++ ClientApp/src/app/income-summary/income-summary.component.ts
@@ -1,64 +1,102 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { CurrencyPipe } from '@angular/common';
-import 'rxjs/add/operator/retry'; 
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import 'rxjs/add/operator/retry';
+import { AgGridNg2 } from 'ag-grid-angular';
+import { IncomeSummaryService } from '../income-summary/income-summary.service';
+import { IncomeSummary } from '../income-summary/income-summary';
+
 
 @Component({
   selector: 'income-summary-data',
-  templateUrl: './income-summary.component.html'
+  templateUrl: './income-summary.component.html',
+  styleUrls: ['./income-summary.component.css']
 })
 
-export class IncomeSummaryComponent {
+export class IncomeSummaryComponent implements OnInit {
     private currentDate: Date = new Date();
     private currentYear: number = this.currentDate.getFullYear();
     public currentYearHeading: number = this.currentYear;
     public ytdIncomeSummary: IncomeSummary[];
-    private apiUrl: string;
-    private httpCilentReference: HttpClient;
     public incomeSummaryTotal: any;
 
 
-    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string)
-    {
-        // 'true' required param used only for WebApi :  GetRevenueSummary() vs. GetRevenue().
-        this.apiUrl = baseUrl + 'api/Income/' + '0' + '/true';
-        this.httpCilentReference = http;
+    constructor(private incomeSummarySvc: IncomeSummaryService)
+    { }
+
+
+    @ViewChild('agGridIncomeSummary')
+    agGridIncomeSummary: AgGridNg2;
+
+
+    ngOnInit() {
         this.processIncomeSummary();
     }
+    
+    columnDefs = [
+        { headerName: "Month recv'd", field: "MonthRecvd", sortable: true, checkboxSelection: true, width: 118, resizable: true },
+        { headerName: "Amount recv'd", field: "AmountRecvd", width: 130, resizable: true, cellStyle: { textAlign: "right" },
+            filter: "agNumberColumnFilter",
+            filterParams: {
+                applyButton: true,
+                clearButton: true,
+                apply: true
+            }
+        },
+        { headerName: "YTD Avg.", field: "YtdAverage", width: 97, resizable: true, cellStyle: { textAlign: "right" } },
+        { headerName: "Rolling 3 Mo. Avg.", field: "Rolling3MonthAverage", width: 152, resizable: true, cellStyle: { textAlign: "right" } },
+    ];
+
+    rowData: any;
 
     onYearSelected(yearsBackDated: number) {
         this.currentYearHeading = this.currentYear - yearsBackDated;
-        this.apiUrl = this.apiUrl.substr(0, this.apiUrl.lastIndexOf("/") - 2) + "/" + yearsBackDated.toString() + "/" + true;
-        this.processIncomeSummary();
+        this.processIncomeSummary(yearsBackDated);
     }
 
-    processIncomeSummary() {
-        // TODO: refactor into seperate service component.
-        this.httpCilentReference.get<any[]>(this.apiUrl)
+
+    processIncomeSummary(backDatedYears: number = 0) {
+        this.incomeSummarySvc.BuildIncomeSummary(backDatedYears)
             .retry(2)
-            .subscribe(result =>
-            {
-                this.ytdIncomeSummary = result;
+            .subscribe(summaryResults => {
+                let mappedSummaryResults = this.mapIncomeSummaryForGrid(summaryResults);
+                this.agGridIncomeSummary.api.setRowData(mappedSummaryResults);
                 let runningTotal: any = 0.0;
                 let ctr: any = 0;
 
-                for (ctr in result) {
-                    runningTotal += parseFloat(result[ctr].amountRecvd);
+                for (ctr in mappedSummaryResults) {
+                    runningTotal += parseFloat(mappedSummaryResults[ctr].AmountRecvd.toString());
                 }
                 this.incomeSummaryTotal = runningTotal;
             },
-            (error: HttpErrorResponse) => alert("Error retreiving income data for year: \n"
-                                                    + this.currentYearHeading
-                                                    + "\ndue to " + error.message)
-            );
+            (error: HttpErrorResponse) => alert("Error retreiving income summary data for year: \n"
+                + this.currentYearHeading
+                + "\ndue to " + error.message)
+        );
     }
 
-}
+
+    mapIncomeSummaryForGrid(recvdSummary: any): IncomeSummary[] {
+
+        if (recvdSummary != null) {
+            let mappedSummaryArr = new Array<IncomeSummary>();
+            for (let idx = 0; idx < recvdSummary.length; idx++) {
+                let modelRecord = new IncomeSummary();
+                modelRecord.MonthRecvd = recvdSummary[idx].monthRecvd;
+                modelRecord.AmountRecvd = (recvdSummary[idx].amountRecvd).toFixed(2);
+                modelRecord.YtdAverage = (recvdSummary[idx].ytdAverage).toFixed(2);
+                modelRecord.Rolling3MonthAverage = (recvdSummary[idx].rolling3MonthAverage).toFixed(2);
+
+                mappedSummaryArr.push(modelRecord);
+            }
+            return mappedSummaryArr;
+        } else
+            return null;
+       
+       
+    }
 
 
-interface IncomeSummary {
-  MonthRecvd: number;
-  AmountRecvd: number;
-  YtdAverage: number | CurrencyPipe;
-  Rolling3MonthAverage: number | CurrencyPipe; 
 }
+
+  
+
