834181d88f222a1f1ea84c04652480abb660a577
Richard P. Asch <rpasch@rpclassics.net>
2019-05-20 10:23:25 -0700

    Initial creation.

---
+++ ClientApp/src/app/shared/investor.ts
@@ -0,0 +1,10 @@
+export class Investor {
+
+    id: number;
+    investorname: string; // login name
+    password: string;
+    firstName: string;    // account name
+    lastName: string;
+    token: string;
+
+}
