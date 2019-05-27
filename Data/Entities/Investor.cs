65e447809f3c37bf9907a05c36f06ec50325e60f
Richard P. Asch <rpasch@rpclassics.net>
2019-05-27 15:49:52 -0700

    Updated to accommodate Registration functioanlity; ; Registration tested Ok.

--- Data/Entities/Investor.cs
+++ Data/Entities/Investor.cs
@@ -7,18 +7,14 @@ namespace PIMS3.Data.Entities
 {
     public class Investor
     {
-        [Required]
-        public string InvestorId { get; set; }
 
-
-        // M:M relationship
+        // Required M:M relationship
         public IList<AssetInvestor> AssetInvestors { get; set; }
 
 
-        // ** NOTE TODO ** 
-        // 5.20.19 - Commented '[Required]' annotated fields should be considered for elimination, as they may no  
-        //           longer be necessary for new security implementation! Check for current usage.
-        //           Exception: InvestorId.
+        [Required]
+        public string InvestorId { get; set; }
+
 
         [Required]
         public  string LastName { get; set; }
@@ -28,44 +24,16 @@ namespace PIMS3.Data.Entities
         public  string FirstName { get; set; }
 
 
-        public string MiddleInitial { get; set; }
-
-
-        public string BirthDay { get; set; }
-
-
-        //[Required]
-        public string Address1 { get; set; }
-
-
-        public string Address2 { get; set; }
-
-        //[Required]
-        public  string City { get; set; }
-
-
-        //[Required]
-        public string State { get; set; }
-
-
-        //[Required]
-        public string ZipCode { get; set; }
-
-
-        public string Phone { get; set; }
-
-
-        public string Mobile { get; set; }
-        
         [Required]
         public  string LoginName { get; set; }
 
 
         //[Required]
-        public string Password { get; set; }
+        public byte[] PasswordHash { get; set; }
 
 
-        public string Token { get; set; }
+        //[Required]
+        public byte[] PasswordSalt { get; set; }
 
 
     }
