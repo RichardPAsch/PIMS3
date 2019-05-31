f4f1cebb78c4578e3aaebc31c7c64a555184a4a2
Richard P. Asch <rpasch@rpclassics.net>
2019-05-31 10:37:19 -0700

    Added Role attribute, for authorization .

--- Data/Entities/Investor.cs
+++ Data/Entities/Investor.cs
@@ -5,9 +5,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PIMS3.Data.Entities
 {
+    /* 
+       'Investor' entity represents data for an investor within PIMS, and can be used to pass data between 
+       different parts of the application (e.g. between services and controllers), as well as returning 
+       http response data from controller action methods.
+    */
+
     public class Investor
     {
-
         // Required M:M relationship
         public IList<AssetInvestor> AssetInvestors { get; set; }
 
@@ -34,6 +39,9 @@ namespace PIMS3.Data.Entities
 
         //[Required]
         public byte[] PasswordSalt { get; set; }
+
+
+        public string Role { get; set; }
 
 
     }
