1b88ca1a41f327d17ccdb4aa247af2ef2aee419f
Richard P. Asch <rpasch@rpclassics.net>
2019-05-31 10:35:38 -0700

    Initial creation.

---
+++ Data/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PIMS3.Data.Entities
+{
+    public static class Role
+    {
+        // Defines all the roles within PIMS.
+
+        public const string Investor = "Investor";
+        public const string Admin = "Admin";
+    }
+}
