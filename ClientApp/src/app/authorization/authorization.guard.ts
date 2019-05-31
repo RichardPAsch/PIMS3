b0b4ab4dafc23460345ef94f63baf9a9de84cf71
Richard P. Asch <rpasch@rpclassics.net>
2019-05-31 15:11:21 -0700

    Initial creation to accommodate security.

---
+++ ClientApp/src/app/authorization/authorization.guard.ts
@@ -0,0 +1,34 @@
+
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from '../shared/authentication.service';
+
+/*  Note:
+    This authorization check guards against unauthorized investors from accessing restricted routes, via CanActivate(). Checks if a route can be activated,
+    returning true, allowing access to proceed, if ok, otherwise returning false, blocking route access.
+    This uses the authentication service to check if the investor is logged in, and if so, checks if their role is authorized to access the requested route.
+    If a investor is logged in and authorized, canActivate() returns true, otherwise it returns false, redirecting to the login page.
+*/
+
+@Injectable({ providedIn: 'root' })
+export class AuthorizationGuard implements CanActivate {
+    constructor(private router: Router, private authSvc: AuthenticationService) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+        let currentInvestor = this.authSvc.currentInvestorValue;
+        if (currentInvestor) {
+            // Check if route is restricted by role.
+            if (route.data.roles && route.data.roles.indexOf(currentInvestor.role) == -1) {
+                 // Role unauthorized.
+                this.router.navigate(['/']);
+                return false;
+            }
+            return true;
+        }
+
+        // Not logged in.
+        this.router.navigate(['/']);
+        return false;
+    }
+}
---
+++ ClientApp/src/app/authorization/role.enum.ts
@@ -0,0 +1,4 @@
+export enum Role {
+    investor = "Investor",
+    admin = "Admin"
+}
