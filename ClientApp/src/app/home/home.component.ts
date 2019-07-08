2351c65888ed035f21b8b0519ad71eafc9c1ff3d
Richard P. Asch <rpasch@rpclassics.net>
2019-07-08 12:33:52 -0700

    Added error handing to onSubmit().

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AuthenticationService } from '../shared/authentication.service';
 // TODO: import { AlertService } from '@/_services';
-import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 
 @Component({
-  selector: 'app-home',
+    selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
     
 export class HomeComponent implements OnInit {
 
-    constructor(private router: Router, private authenticationSvc : AuthenticationService, private navMenuComp: NavMenuComponent) {
+    constructor(private router: Router, private authenticationSvc : AuthenticationService) {
         // TODO: Add as params:   private alertSvc: AlertService
-
+      
         if (this.authenticationSvc.currentInvestorValue) {
             alert("Login already exist for: \n" + this.authenticationSvc.currentInvestorValue.firstName
                                                 + " "
@@ -24,13 +23,18 @@ export class HomeComponent implements OnInit {
             this.router.navigate(['/income-summary']);
         }
     }
+
  
     // Original investorId for rpasch@rpclassics.net: CF256A53-6DCD-431D-BC0B-A810010F5B88
     loading = false;
     submitted = false;
     returnUrl: string;
+    //loginForm = new FormGroup({
+    //    investorName: new FormControl('rpasch@rpclassics.net', [Validators.required]), 
+    //    password: new FormControl('rich25102', [Validators.required, Validators.minLength(6)]),
+    //});
     loginForm = new FormGroup({
-        investorName: new FormControl('jbrahms@gmail.com', [Validators.required]), 
+        investorName: new FormControl('jbrahms@gmail.com', [Validators.required]),
         password: new FormControl('classical1', [Validators.required, Validators.minLength(6)]),
     });
     investorName: string;
@@ -58,15 +62,16 @@ export class HomeComponent implements OnInit {
             .pipe(first())
             .subscribe(investorModel =>
             {
-                this.navMenuComp.initializeDisplayName = investorModel.firstName; 
                 this.investorName = investorModel.firstName;
                 this.router.navigate(['/income-summary']);
             },
-            error => {
-                alert("Error with login, due to: " + error(error));
+            () => {
                 //this.alertService.error(error);
                 this.loading = false;
+                alert("Unable to validate login credentials, \nplease check name and/or password entry(ies).");
             });
+
+          
     }
 
 }
