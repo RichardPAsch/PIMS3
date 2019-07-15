9ffbc0e152f6bf0b6de503bdc977fd7a3066f2ea
Richard P. Asch <rpasch@rpclassics.net>
2019-07-15 14:29:56 -0700

    Expanded ParseAccountTypeFromDescription() to include new account types.

--- Services/CommonSvc.cs
+++ Services/CommonSvc.cs
@@ -24,17 +24,21 @@ namespace PIMS3.Services
                        .ToString();
         }
 
-        // From: C:\Development\PIMS_References\PIMS.WebApi\Utilities.cs
+
         public static string ParseAccountTypeFromDescription(string accountDesc)
         {
-            // Any account description that includes superfluous data, e.g.,account number, will
-            // be consolidated to the appropriate account type. Primarily used during XLS
+            // Any received account description that includes superfluous data, e.g.,account number,
+            // will be truncated to the appropriate account type. Primarily used during XLSX data import
             // revenue processing.
 
             if (accountDesc.ToUpper().IndexOf("IRA", StringComparison.Ordinal) >= 0 && accountDesc.ToUpper().IndexOf("ROTH", StringComparison.Ordinal) == -1)
                 return "IRA";
             if (accountDesc.ToUpper().IndexOf("ROTH", StringComparison.Ordinal) >= 0 && accountDesc.ToUpper().IndexOf("IRA", StringComparison.Ordinal) >= 0)
                 return "ROTH-IRA";
+            if (accountDesc.ToUpper().IndexOf("401", StringComparison.Ordinal) >= 0)
+                return "401(k)";
+            if (accountDesc.ToUpper().IndexOf("KEOGH", StringComparison.Ordinal) >= 0)
+                return "KEOGH";
 
             return accountDesc.ToUpper().IndexOf("CMA", StringComparison.Ordinal) >= 0 ? "CMA" : null;
         }
