39a03b4bfe0259e555e96ee57cfb71f81b594d2a
Richard P. Asch <rpasch@rpclassics.net>
2019-08-14 09:38:19 -0700

    Added Update().

--- DataAccess/Investor/InvestorDataProcessing.cs
+++ DataAccess/Investor/InvestorDataProcessing.cs
@@ -21,70 +21,27 @@ namespace PIMS3.DataAccess.Investor
         }
 
 
-        //public string RetreiveIdByName(string investorName)
-        //{
-        //    //var fetchedInvestor = Retreive(i => i.EMailAddr.Trim() == investorName.Trim());
-        //    //return fetchedInvestor.FirstOrDefault().InvestorId.ToString();
-
-        //    return _ctx.Investor
-        //               .Where(investor => investor.EMailAddr.Trim() == investorName.Trim())
-        //               .FirstOrDefault().InvestorId
-        //               .ToString();
-        //}
+        public Data.Entities.Investor RetreiveByName(string investorLoginName)
+        {
+            return _ctx.Investor
+                       .Where(investor => investor.LoginName == investorLoginName.Trim())
+                       .FirstOrDefault();
+        }
 
 
+        public bool Update(Data.Entities.Investor investorToUpdate)
+        {
+            int updateCount = 0;
+            _ctx.Update(investorToUpdate);
+            updateCount = _ctx.SaveChanges();
 
-
-        // ** 9.17.18 - To be implemented, pending security implementation ? Any changes made need to be added to interface!  **
-
-        /*
-           public IQueryable<Investor> RetreiveAll()
-           {
-               var investorQuery = (from investor in _nhSession.Query<Investor>() select investor);
-               return investorQuery.AsQueryable();
-               return null;
-           }
-
-           public Investor RetreiveById(string idGuid)
-           {
-               //return _nhSession.Get<Investor>(idGuid);
-               return null;
-           }
-
-
-           public IQueryable<Investor> Retreive(Expression<Func<Investor, bool>> predicate)
-           {
-               return RetreiveAll().Where(predicate);
-           }
-
-           public bool Create(Investor newEntity) {
-               return false;
-           }
-
-
-           public bool Update(Investor entity, object id) {
-
-
-               return true;
-           }
-
-
-           public bool Delete(Guid cGuid) {
-
-               var deleteOk = true;
-               var accountTypeToDelete = RetreiveById(cGuid);
-
-               if (accountTypeToDelete == null)
-                   return false;
-
-
-
-
-               return deleteOk;
-           }
-       */
+            return updateCount == 1
+                ? true
+                : false;
+        }
+       
 
     }
 
-    }
+}
 
