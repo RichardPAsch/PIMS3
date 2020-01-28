5cde8687e1fa901ad5bf15931116ed81ffc12b8b
Richard P. Asch <rpasch@rpclassics.net>
2020-01-27 16:08:03 -0800

    Initial creation to accommodate acct type drop down in 'Positions'.

---
+++ Controllers/AccountTypesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PIMS3.Data;
+using PIMS3.DataAccess.Account;
+using System.Linq;
+
+
+namespace PIMS3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AccountTypesController : ControllerBase
+    {
+        private readonly PIMS3Context _context;
+
+        public AccountTypesController(PIMS3Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IQueryable<string> GetAccountTypes()
+        {
+            AccountDataProcessing accountTypeDataAccessComponent = new AccountDataProcessing(_context);
+            return accountTypeDataAccessComponent.GetAllAccountTypes();
+        }
+    }
+}
