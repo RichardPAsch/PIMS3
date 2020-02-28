4e9679e3f42c670422920272a32ca859d929e0dd
Richard P. Asch <rpasch@rpclassics.net>
2020-02-28 12:52:32 -0800

    Initial creation.

---
+++ Data/Entities/DelinquentIncome.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PIMS3.Data.Entities
+{
+    // Receptacle for overdue revenue, utilized via "Income due".
+
+    public class DelinquentIncome
+    {
+        [Required]
+        [Key]
+        public string PositionId { get; set; }
+
+        [Required]
+        [Key]
+        public string MonthDue { get; set; }
+
+        [Required]
+        public string InvestorId { get; set; }
+
+        [Required]
+        public string TickerSymbol { get; set; }
+
+        [Required]
+        public string AccountTypeDesc { get; set; }
+
+
+    }
+}
