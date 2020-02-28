16826e3db6be1c9ca8307c03c88dd39479667c46
Richard P. Asch <rpasch@rpclassics.net>
2020-02-28 12:24:51 -0800

    Added 'monthDue' to accommodate delinquent receivables.

--- ClientApp/src/app/income-receivables/receivable.ts
+++ ClientApp/src/app/income-receivables/receivable.ts
@@ -2,6 +2,7 @@
 export class Receivable {
     positionId: string;
     tickerSymbol: string;
+    monthDue: string;
     accountTypeDesc: string;
     dividendPayDay: number;
     dividendFreq: string;
