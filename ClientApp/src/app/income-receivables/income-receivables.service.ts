4c55684f0dd1bba4a4abc46367bb11588f91908e
Richard P. Asch <rpasch@rpclassics.net>
2020-04-09 11:01:40 -0700

    Removed unused httpHeaders & httpOptions.

--- ClientApp/src/app/income-receivables/income-receivables.service.ts
+++ ClientApp/src/app/income-receivables/income-receivables.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GlobalsService } from '../shared/globals.service';
 
-let httpHeaders = new HttpHeaders()
-    .set('Content-Type', 'application/json')
-    .set('Cache-Control', 'no-cache');
-
-const httpOptions = {
-    headers: httpHeaders
-};
 
 
 @Injectable({
