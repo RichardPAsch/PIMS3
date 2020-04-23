792e82dcedcd8669182ac5941fa7c8edc1f9eb2f
Richard P. Asch <rpasch@rpclassics.net>
2020-04-23 11:06:37 -0700

    Removed commented code.

--- ViewModels/AssetCreationVm.cs
+++ ViewModels/AssetCreationVm.cs
@@ -35,40 +35,4 @@ namespace PIMS3.ViewModels
     }
 }
 
-/* Original code:
- 
- * public string AssetInvestorId { get; set; }                     // Map -> Asset
-      
-        [Required]
-        public string AssetTicker { get; set; }                         // Map -> Profile
 
-
-        [Required]
-        public string AssetDescription { get; set; }                    // Map -> Profile
-
-        [Required]
-        public string AssetClassification { get; set; }                 // Map -> AssetClass; recvd from client
-        
-
-        public string AssetClassificationId { get; set; }               // Map -> Asset; persist to Db
-        
-
-        public ProfileVm ProfileToCreate { get; set; }
-        
-        
-        public PositionVm PositionToCreate { get; set; }                // includes AccountType & Transaction
-
-
-        [Required]                                                      // added 5.10.16 for model state validation check
-        public List<PositionVm> PositionsCreated { get; set; }
-
-
-        public string AssetIdentification { get; set; }                 // Map -> Position Asset ref.
- 
-
-        public IncomeVm IncomeToCreate { get; set; }
-
-
-        public List<IncomeVm> RevenueCreated { get; set; }              // Optional
- 
- */
