860bdd5c5a24e8bc48d037068aee8f981ea2f27c
Richard P. Asch <rpasch@rpclassics.net>
2020-07-08 10:56:09 -0700

    To be added. Not created by me.

---
+++ ClientApp/src/app/error-logging/logException.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var LogException = /** @class */ (function () {
+    function LogException() {
+    }
+    return LogException;
+}());
+exports.LogException = LogException;
+//# sourceMappingURL=logException.js.map
