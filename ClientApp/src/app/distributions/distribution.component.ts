9f5f71604650a562d87de85c8bfe1faff538fcac
Richard P. Asch <rpasch@rpclassics.net>
2021-04-21 16:34:56 -0700

    Added mapDistributionsForGrid(recvdDistribution() & calculateDistributionCounts() in addition to grid adjustments.

--- ClientApp/src/app/distributions/distribution.component.ts
+++ ClientApp/src/app/distributions/distribution.component.ts
@@ -3,6 +3,9 @@ import { AgGridNg2 } from 'ag-grid-angular';
 import { HttpErrorResponse } from '@angular/common/http';
 import { takeUntil } from 'rxjs/operators';
 import { BaseUnsubscribeComponent } from '../base-unsubscribe/base-unsubscribe.component';
+import { Distribution } from '../distributions/distribution';
+import { ProfileService } from '../shared/profile.service';
+import { AlertService } from '../shared/alert.service';
 
 @Component({
     selector: 'app-distributions',
@@ -11,14 +14,91 @@ import { BaseUnsubscribeComponent } from '../base-unsubscribe/base-unsubscribe.c
 })
 export class DistributionsComponent extends BaseUnsubscribeComponent implements OnInit {
 
-    constructor() {
+    
+    constructor(private profileSvc: ProfileService, private alertSvc: AlertService) {
         super();
     }
 
     @ViewChild('agGridDistributions', { static: false })
     agGridDistributions: AgGridNg2;
 
+    public quarterlyCount: number = 0;
+    public monthlyCount: number = 0;
+    public semi_annualCount: number = 0;
+    public annualCount: number = 0;
+
+
     ngOnInit() {
+        this.getDistributionSchedules();
+    }
+
+    columnDefs = [
+        { headerName: "Ticker", field: "tickerSymbol", sortable: true, filter: true, width: 145, resizable: true },
+        { headerName: "Frequency", field: "frequency", sortable: true, filter: true, width: 105, resizable: true },
+        { headerName: "Month(s) Paid", field: "months", filter: true, width: 125, resizable: true },
+        { headerName: "Day", field: "day", sortable: true, filter: true, width: 75, resizable: true },
+    ];
+
+    rowData: any; 
+
+
+    public getDistributionSchedules(): any {
+
+        this.profileSvc.fetchDistributionSchedules()
+            .retry(2)
+            .pipe(takeUntil(this.getUnsubscribe()))
+            .subscribe(responseSchedules => {
+                this.calculateDistributionCounts(responseSchedules);
+                this.agGridDistributions.api.setRowData(this.mapDistributionsForGrid(responseSchedules));
+            },
+            (apiError: HttpErrorResponse) => {
+                if (apiError.error instanceof Error) {
+                    this.alertSvc.error("Error retreiving profile dividend schedules; possible network issue.");
+                }
+                else {
+                    this.alertSvc.error("Error retreiving profile dividend schedules. Please try later.");
+                }
+            }
+        )
+    }
+
+    mapDistributionsForGrid(recvdDistributions: any) : Distribution[] {
+
+        let mappedDistributions = new Array<Distribution>();
+        for (let i = 0; i < recvdDistributions.length; i++) {
+            let distributionModelRecord = new Distribution();
+
+            distributionModelRecord.tickerSymbol = recvdDistributions[i].tickerSymbol;
+            distributionModelRecord.frequency = recvdDistributions[i].distributionFrequency;
+            distributionModelRecord.months = recvdDistributions[i].distributionMonths;
+            distributionModelRecord.day = recvdDistributions[i].distributionDay;
+
+            mappedDistributions.push(distributionModelRecord);
+        }
+
+        return mappedDistributions;
+    }
+
+    calculateDistributionCounts(sourceData: any) {
+
+        for (let i = 0; i < sourceData.length; i++) {
+
+            switch (sourceData[i].distributionFrequency) {
+                case "M":
+                    this.monthlyCount += 1;
+                    break;
+                case "Q":
+                    this.quarterlyCount += 1;
+                    break;
+                case "S":
+                    this.semi_annualCount += 1;
+                    break;
+                case "A":
+                    this.annualCount += 1;
+                    break;
+            }
+        }
 
     }
+
 }
