16f91d2a4d70f33c3b6864d144084bcfaae4ea21
Richard P. Asch <rpasch@rpclassics.net>
2021-04-26 11:44:25 -0700

    Added sortDivPayMonthsValue() for parsing any asset profile divMonths.

--- ClientApp/src/app/shared/globals.service.ts
+++ ClientApp/src/app/shared/globals.service.ts
@@ -4,9 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GlobalsService {
-    /* Provides any application-wide global value(s) that may currently,
-       or in the future, be necessary.
-    */
+
+    /* Provides any application-wide global value(s) needed currently, or in the future. */
 
     constructor() { }
 
@@ -14,6 +13,22 @@ export class GlobalsService {
 
     public get pimsBaseUrl(): string {
         return this.baseUrl;
+    }
+
+
+    sortDivPayMonthsValue(recvdInfo: string): string {
+
+        if (recvdInfo != "N/A") {
+            let monthsStringArray = recvdInfo.split(',');
+            let monthsNumberArray: number[] = new Array();
+            for (let i = 0; i < monthsStringArray.length; i++) {
+                monthsNumberArray[i] = parseInt(monthsStringArray[i]);
+            }
+
+            return monthsNumberArray.sort((a, b) => a - b).toString();
+        } else {
+            return "N/A";
+        }
     }
 
 }
