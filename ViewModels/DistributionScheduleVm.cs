73cf68c211a2933841a01bb8d63b7957030edf0d
Richard P. Asch <rpasch@rpclassics.net>
2021-04-28 14:12:09 -0700

    Initial creation.

---
+++ ViewModels/DistributionScheduleVm.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PIMS3.ViewModels
+{
+    public class DistributionScheduleVm
+    {
+        public string TickerSymbol { get; set; }
+        public string DistributionFrequency { get; set; }
+        public string DistributionMonths { get; set; }
+        public int DistributionDay { get; set; }
+    }
+}
